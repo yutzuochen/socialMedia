f3274eeec84eff2cc86bae3ec93c9cf0626b469d
Your Name <you@example.com>
2020-04-14 17:13:40 +0800

    make user able to create an account

--- account/urls.py
+++ account/urls.py
@@ -1,9 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    #path('login/', views.user_login, name='login'),
+		#path('login/', views.user_login, name='login'),
 		path('login/',auth_views.LoginView.as_view(),name='login'),
 		path('logout/',auth_views.LogoutView.as_view(),name='logout'),
 		path('', views.dashboard, name='dashboard'),
@@ -17,5 +17,8 @@ urlpatterns = [
 		path('password_reset/done/', auth_views.PasswordResetDoneView.as_view(), name='password_reset_done'),
 		path('reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
 		path('reset/done/', auth_views.PasswordResetCompleteView.as_view(), name='password_reset_complete'),
+		
+		path('', include('django.contrib.auth.urls')),
 
+		path('register/', views.register, name='register'),
 ]
